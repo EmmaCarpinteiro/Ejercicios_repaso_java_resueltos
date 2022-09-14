da5e4336c60aff47524902b055f4e548c1465635
EmmaCarpinteiro <104421560+EmmaCarpinteiro@users.noreply.github.com>
2022-09-14 13:04:58 +0200

    EjercicioCinco añadido

---
+++ EjercicioCinco.java
@@ -0,0 +1,13 @@
+package until;
+
+public class EjercicioCinco {
+
+    public static void parImpar(int numero) {
+    	
+        if (numero % 2 == 0) {
+            System.out.println("Par");
+        } else {
+            System.out.println("Impar");
+        }
+    }
+}

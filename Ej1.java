cb3fc3d2168178c7351838107e9a992cd9e039ee
EmmaCarpinteiro <104421560+EmmaCarpinteiro@users.noreply.github.com>
2022-09-14 13:02:40 +0200

    Ejercicio1 añadido

---
+++ Ej1.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class Ej1 {
+    private static String nome = "Emma";
+    private static int idade = 23;
+
+    public static void main(String[] args) {
+        System.out.println("Los datos del empleado son: " + nome + ", " + idade);
+    }
+}
